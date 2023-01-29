3771f1d519f79bf9f8139ee90d1e1f7ae83a8d2d
Karina Radzevich <karina_radzevich@bk.ru>
2023-01-30 03:52:07 +0400

    added task6

---
+++ task6/Program.cs
@@ -0,0 +1,22 @@
+﻿//  Напишите программу, которая на вход принимает число и выдаёт,
+//является ли число чётным (делится ли оно на два без остатка).
+//4 -> да
+//-3 -> нет
+//7 -> нет
+Console.WriteLine("При вводе числа программа покажет,является ли целое число четным");
+Console.WriteLine("Введите целое число");
+int numberA = int.Parse(Console.ReadLine());
+if (numberA % 2 == 0)
+{
+    Console.WriteLine("Ответ: ");
+    Console.Write(numberA);
+    Console.Write(" -> ");
+    Console.Write("да");
+}
+if (numberA % 2 == 1)
+{
+    Console.WriteLine("Ответ: ");
+    Console.Write(numberA);
+    Console.Write(" -> ");
+    Console.Write("нет");
+}
