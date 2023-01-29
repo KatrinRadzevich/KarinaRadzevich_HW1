e6e87992f4ed6c3e86349eeace70b9179093cc47
Karina Radzevich <karina_radzevich@bk.ru>
2023-01-30 00:33:24 +0400

    added renovations in task 2

--- task2/Program.cs
+++ task2/Program.cs
@@ -3,18 +3,37 @@
 //a = 5; b = 7 -> max = 7
 //a = 2 b = 10 -> max = 10
 //a = -9 b = -3->max = -3
-Console.WriteLine("Введите одно целое число");
+Console.WriteLine("Вам нужно ввести 2 разных целых числа и программа выдаст максимальное и минимальное из них");
+Console.WriteLine("Введите первое целое число");
 int numberA = int.Parse(Console.ReadLine());
-Console.WriteLine("Введите еще одно целое число");
+Console.WriteLine("Введите второе целое число");
 int numberB = int.Parse(Console.ReadLine());
 int max = numberA;
 if (numberA > numberB)
 {
-    Console.Write("max= ");
+    Console.WriteLine("Ответ: ");
+    Console.Write("a = ");
     Console.Write(numberA);
+    Console.Write("; ");
+    Console.Write("b = ");
+    Console.Write(numberB);
+    Console.Write(" -> ");
+    Console.Write(" max = ");
+    Console.Write(numberA);
+    Console.Write(" , min = ");
+    Console.Write(numberB);
 }
 if (numberA < numberB)
 {
-    Console.Write("max= ");
+    Console.WriteLine("Ответ: ");
+    Console.Write("a = ");
+    Console.Write(numberA);
+    Console.Write("; ");
+    Console.Write("b = ");
     Console.Write(numberB);
+    Console.Write(" -> ");
+    Console.Write(" max = ");
+    Console.Write(numberB);
+    Console.Write(" , min = ");
+    Console.Write(numberA);
 }
